7c84f2a068a5266b150e63cbae11aa302c0d99a9
kasymjaparov <kasymjaparov@gmail.com>
2021-10-10 23:14:48 +0600

    add responsivity for header menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,6 +14,20 @@ const Header = () => {
       setChangeTime(new Date())
     }, milliseconds)
   }, [changeTime])
+  const [show, setShow] = React.useState(true)
+  React.useEffect(() => {
+    if (window.matchMedia("(max-width:650px)").matches) {
+      setShow(false)
+    }
+  }, [])
+  React.useEffect(() => {
+    if (show) {
+      document.querySelector("body").style.overflow = "hidden"
+    } else {
+      document.querySelector("body").style.overflow = "auto"
+    }
+  }, [show])
+  console.log(show)
   const links = [
     { href: "/aboutus", text: "О нас" },
     { href: "/rent", text: "Аренда" },
@@ -26,28 +40,53 @@ const Header = () => {
       <NavLink to='/' className={"header__title"}>
         <img src={logo} alt='logo bike park' />{" "}
       </NavLink>
-      <div className='header__links'>
-        {links.map(item => (
+      {show && (
+        <nav>
+          <div className='header__links'>
+            {links.map(item => (
+              <NavLink
+                activeClassName='header__links-item-active'
+                to={item.href}
+                onClick={() => {
+                  if (window.matchMedia("(max-width:650px)").matches)
+                    setShow(!show)
+                }}
+                className='header__links-item'
+              >
+                {item.text}
+              </NavLink>
+            ))}
+          </div>
           <NavLink
-            activeClassName='header__links-item-active'
-            to={item.href}
-            className='header__links-item'
+            onClick={() => {
+              if (window.matchMedia("(max-width:650px)").matches) setShow(!show)
+            }}
+            to='/auth'
+            className={"header__authLinks"}
           >
-            {item.text}
+            <img src={login} alt='login' />
+            <span>Личный кабинет</span>
           </NavLink>
-        ))}
-      </div>
-      <NavLink to='/auth' className={"header__authLinks"}>
-        <img src={login} alt='login' />
-      </NavLink>
-      <div className={"header__date"}>
-        {changeTime.getHours() < 10
-          ? `0${changeTime.getHours()}`
-          : changeTime.getHours()}
-        :
-        {changeTime.getMinutes() < 10
-          ? `0${changeTime.getMinutes()}`
-          : changeTime.getMinutes()}
+        </nav>
+      )}
+      <div className='header_date_burger'>
+        <div className={"header__date"}>
+          {changeTime.getHours() < 10
+            ? `0${changeTime.getHours()}`
+            : changeTime.getHours()}
+          :
+          {changeTime.getMinutes() < 10
+            ? `0${changeTime.getMinutes()}`
+            : changeTime.getMinutes()}
+        </div>
+        <div
+          onClick={() => setShow(!show)}
+          className={!show ? "burger" : "burger-open burger"}
+        >
+          <div className='burger_item' />
+          <div className='burger_item' />
+          <div className='burger_item' />
+        </div>
       </div>
     </div>
   )
